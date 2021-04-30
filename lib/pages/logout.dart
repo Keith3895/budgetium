268829cebca0785d97b849396279aa454182e3cb
keith3895 <keith30895@gmail.com>
2021-04-30 22:45:12 +0530

    design complete

---
+++ lib/pages/logout.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class Logout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   backgroundColor: Colors.transparent,
+      // ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height * .90,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'BUDGETIUM',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        fontSize: 36,
+                        fontFamily: 'Inter'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      'A budgeting tool to plan your expenses',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 24,
+                          fontFamily: 'Inter'),
+                    ),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/logout_bg.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              buttonPadding: EdgeInsets.symmetric(horizontal: 15),
+              buttonMinWidth: 100,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('LOGIN'),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Color(0xFF6200EE))),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('REGISTER'),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Color(0xFF6200EE))),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Photo by <a href="https://unsplash.com/@kharp?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Katie Harp</a> on <a href="https://unsplash.com/s/photos/money?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
