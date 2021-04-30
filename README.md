9061a4129daed0abe7a35a55b98de16411608696
keith3895 <keith30895@gmail.com>
2021-04-30 18:19:27 +0530

    init comit

---
+++ README.md
@@ -0,0 +1,7 @@
+# budgetium
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
---
+++ android/app/src/main/kotlin/com/example/budgetium/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.budgetium
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
