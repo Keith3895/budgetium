6cb8ae6d3a0a20312dc8a9764b76f5b558dfac9f
keith3895 <keith30895@gmail.com>
2021-05-06 21:57:45 +0530

    fixed spelling mistake

--- test/widget_tests/logout_test.dart
+++ test/widget_tests/logout_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:budgetium/pages/Logout.dart';
+import 'package:budgetium/pages/logout.dart';
 
 void main() {
   Widget createWidgetForTesting({Widget? child}) {
